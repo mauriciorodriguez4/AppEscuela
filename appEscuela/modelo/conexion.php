e0acaf94cacf29403f37c300e2d22272b7396d6e
mauriciorodriguez4 <98993011+mauriciorodriguez4@users.noreply.github.com>
2023-11-09 16:25:45 -0600

    Delete appEscuela directory

--- appEscuela/modelo/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost","root","","app_escuela");
-//$conexion -> set_charset("utf8");
-//if(!$conexion) {
-  //  die("No se pudo conectar a la base de datos");
-//}else{
-  //  echo "Conexion exitosa";
-//}
-?>
--- appEscuela/vista/navbarV.php
+++
@@ -1,8 +0,0 @@
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light" id="navDesignLogin">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="../admin.php">
-            <img src="img/mined.png" style="width: 200px; height: 90px;" alt="logoMined">
-        </a>        
-    </div>
-</nav>
